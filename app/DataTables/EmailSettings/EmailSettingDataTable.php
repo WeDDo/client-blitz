50745eb67853ecc0f81f3915991bef64f8630f9a
Mantas <mantuxas001@gmail.com>
2025-03-12 22:43:47 +0200

    Save current grid page Status: Open. #9

--- app/DataTables/EmailSettings/EmailSettingDataTable.php
+++ app/DataTables/EmailSettings/EmailSettingDataTable.php
@@ -3,6 +3,7 @@
 namespace App\DataTables\EmailSettings;
 
 use App\DataTables\BaseDataTable;
+use App\Models\DataTable;
 use App\Models\User;
 use Illuminate\Pagination\LengthAwarePaginator;
 
@@ -10,10 +11,30 @@ class EmailSettingDataTable extends BaseDataTable
 {
     public function getData(): LengthAwarePaginator
     {
+        $page = request('page');
+        if (!$page) {
+            $dataTable = DataTable::query()
+                ->where('name', $this->name)
+                ->where('user_id', auth()->id())
+                ->first();
+
+            $page = $dataTable->page ?? 1;
+        }
+
+        DataTable::query()->updateOrCreate(
+                [
+                    'name' => $this->name,
+                    'user_id' => auth()->id(),
+                ],
+                [
+                    'page' => $page,
+                ]
+            );
+
         return User::query()
             ->find(auth()->id())
             ->emailSettings()
             ->orderBy('created_at', 'desc')
-            ->paginate($this->perPage);
+            ->paginate($this->perPage, page: $page);
     }
 }
